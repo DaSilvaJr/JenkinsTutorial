87f74c60e4d1cff1dfd85535d1cfd62049e2bbde
Ibnu Aziz Nurrohman <ibnuaziznu@gmail.com>
2017-12-03 08:11:04 +0700

    Second Commit

--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 var supertest = require("supertest");
 var should = require("should");
-
+var mysql = require("mysql");
 var server = supertest.agent("http://localhost:4000");
+var expect = require('chai').expect;
 
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'simac'
+});
 describe("SAMPLE unit test",function(){
 
   it("should return home page",function(done){
@@ -17,4 +24,7 @@ describe("SAMPLE unit test",function(){
      done();
   });
 
+  it('Should succes connected', function(){
+           expect(typeof(con)=='object').to.be.true;
+        });
 });
